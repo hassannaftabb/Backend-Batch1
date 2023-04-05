7be03031b9f2267448ffd4ef7457df7f09f13574
Hassan Aftab <hassannaftabb@gmail.com>
2023-04-05 16:45:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//To create a package.json file or initialize a node js project, you need to run commmand
+// "npm init"
+
+// JSON stands for Javascript Object Notation, It is a format of data
+// which is used to transfer the data from server to client, or client to server
+
+//Asynchronous
