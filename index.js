cf5d9b9cd9cfccba0e4eb97dfdf0ea2a278a4499
Hassan Aftab <hassannaftabb@gmail.com>
2023-04-07 16:30:21 +0500

    todays work

--- index.js
+++ index.js
@@ -4,4 +4,13 @@
 // JSON stands for Javascript Object Notation, It is a format of data
 // which is used to transfer the data from server to client, or client to server
 
-//Asynchronous
+// fs - File System
+//To Read a File - readFile(synchronous), readFileSync(asynchronous)
+const fileSystem = require("fs");
+
+console.log("Before");
+fileSystem.readFile("hassann.txt", "utf-8", (err, data) => {
+  console.log("Error:", err);
+  console.log("Data:", data);
+});
+console.log("After");
